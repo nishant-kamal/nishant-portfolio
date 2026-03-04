19038acf39c965a6ca244e681aa9c8c966e08d38
nishant-kamal <157911131+nishant-kamal@users.noreply.github.com>
2026-03-04 11:47:52 +0530

    Create Hero.js

---
+++ components/Hero.js
@@ -0,0 +1,19 @@
+export default function Hero() {
+  return (
+    <section style={{ marginBottom: "60px" }}>
+      <h1>Nishant Kamal</h1>
+      <h3>Site Reliability Engineer</h3>
+
+      <p style={{ marginTop: "10px" }}>
+        Site Reliability Engineer with 6+ years of experience operating
+        production infrastructure, improving reliability, and building
+        scalable cloud platforms using Kubernetes and AWS.
+      </p>
+
+      <p style={{ marginTop: "10px" }}>
+        Specialized in distributed systems, CI/CD automation, observability,
+        and infrastructure as code.
+      </p>
+    </section>
+  );
+}
