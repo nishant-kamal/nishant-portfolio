8670c17384a04b55bcb6dac902f14ff8a2b1d791
nishant-kamal <157911131+nishant-kamal@users.noreply.github.com>
2026-03-04 11:49:19 +0530

    Create Contact.js

---
+++ components/Contact.js
@@ -0,0 +1,21 @@
+export default function Contact() {
+  return (
+    <section style={{ marginBottom: "60px" }}>
+      <h2>Contact</h2>
+
+      <p>Email: nishant.kamal2015@gmail.com</p>
+
+      <p>
+        GitHub:
+        <a
+          href="https://github.com/imnishantdevops"
+          target="_blank"
+        >
+          github.com/imnishantdevops
+        </a>
+      </p>
+
+      <p>Location: New Delhi, India</p>
+    </section>
+  );
+}
