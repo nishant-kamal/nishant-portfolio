dae3d91591fdbaca53424d77b93ab3c7d4e74958
Nishant Kamal <157911131+nishant-kamal@users.noreply.github.com>
2026-03-04 16:50:30 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-gray-400 py-10 border-t border-slate-800 mt-20">
+      © 2026 Nishant Kamal
+    </footer>
+  )
+}
