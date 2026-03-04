e30aae438305ebede6e2591cee96886f82299ab0
nishant-kamal <157911131+nishant-kamal@users.noreply.github.com>
2026-03-04 14:11:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-}
+  basePath: "/nishant-portfolio",
+  assetPrefix: "/nishant-portfolio/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
