39aab9d41f4b4fdd69cc9e494fd5060287d554aa
nishant-kamal <157911131+nishant-kamal@users.noreply.github.com>
2026-03-04 11:48:37 +0530

    Create Skills.js

---
+++ components/Skills.js
@@ -0,0 +1,20 @@
+export default function Skills() {
+  return (
+    <section style={{ marginBottom: "60px" }}>
+      <h2>Tech Stack</h2>
+
+      <ul>
+        <li>Kubernetes</li>
+        <li>AWS (EKS, EC2, S3, IAM)</li>
+        <li>Docker</li>
+        <li>Terraform</li>
+        <li>Kafka</li>
+        <li>Debezium</li>
+        <li>FluxCD</li>
+        <li>Prometheus</li>
+        <li>Grafana</li>
+        <li>Istio Service Mesh</li>
+      </ul>
+    </section>
+  );
+}
