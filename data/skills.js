76e8f5ea65465c7824d5610c3176d75e022b8eaf
nishant-kamal <157911131+nishant-kamal@users.noreply.github.com>
2026-03-04 11:55:12 +0530

    Create skills.js

---
+++ data/skills.js
@@ -0,0 +1,42 @@
+export const skills = [
+  {
+    name: "Kubernetes",
+    icon: "/icons/kubernetes.svg"
+  },
+  {
+    name: "AWS",
+    icon: "/icons/aws.svg"
+  },
+  {
+    name: "Kafka",
+    icon: "/icons/kafka.svg"
+  },
+  {
+    name: "Debezium",
+    icon: "/icons/debezium.svg"
+  },
+  {
+    name: "Terraform",
+    icon: "/icons/terraform.svg"
+  },
+  {
+    name: "Docker",
+    icon: "/icons/docker.svg"
+  },
+  {
+    name: "FluxCD",
+    icon: "/icons/fluxcd.svg"
+  },
+  {
+    name: "Prometheus",
+    icon: "/icons/prometheus.svg"
+  },
+  {
+    name: "Grafana",
+    icon: "/icons/grafana.svg"
+  },
+  {
+    name: "Istio",
+    icon: "/icons/istio.svg"
+  }
+]
