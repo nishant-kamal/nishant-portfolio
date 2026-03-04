9eb52a739adc29e17f315591ee68431118ad8a2d
nishant-kamal <157911131+nishant-kamal@users.noreply.github.com>
2026-03-04 12:10:29 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import "./globals.css"
+
 export const metadata = {
   title: "Nishant Kamal | Site Reliability Engineer",
   description:
@@ -8,15 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header style={{ padding: "20px", borderBottom: "1px solid #333" }}>
-          <h2>Nishant Kamal</h2>
-        </header>
-
         {children}
-
-        <footer style={{ padding: "20px", borderTop: "1px solid #333", marginTop: "40px" }}>
-          <p>© {new Date().getFullYear()} Nishant Kamal</p>
-        </footer>
       </body>
     </html>
   );
